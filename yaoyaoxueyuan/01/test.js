1ab001ae8fee9c39f95b6b737bdbaa66a5e8972a
德坤 <1143255840@qq.com>
2017-05-15 22:34:59 +0800

    Add files via upload

---
+++ yaoyaoxueyuan/01/test.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    var before = document.getElementById("before");
+    var mid = document.getElementById("mid");
+    var rear = document.getElementById("rear");
+    befor.onclik = function(){
+    }
+}
